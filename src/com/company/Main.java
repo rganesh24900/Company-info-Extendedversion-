525d76975d09c1910ff4224d7fc4c8b91212ece4
Ganesh <rganesh802@gmail.com>
2021-10-13 20:17:45 +0530

    Modified the project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,18 +11,18 @@ public class Main {
         SalBand sal =new SalBand();
 
 
-        Engineer emp1 = new Engineer("Naheed", "Part-time", sal.engineerLevel1 );
-        Engineer emp2 = new Engineer("Dhanush", "Full-time", sal.engineerLevel3);
-        Engineer emp3 = new Engineer("Rajesh", "Full-time", sal.engineerLevel2 );
+        Employee emp1 = new Engineer("Naheed", "Part-time", sal.engineerLevel1 );
+        Employee emp2 = new Engineer("Dhanush", "Full-time", sal.engineerLevel3);
+        Employee emp3 = new Engineer("Rajesh", "Full-time", sal.engineerLevel2 );
 
 
 
         //Manager's
-        Manager emp4 = new Manager("Salma", "Full-time", sal.managerLevel2);
-        Manager emp5 = new Manager("Amitabh", "Full-time", sal.managerLevel2);
-        Manager emp6 = new Manager("Jitesh", "Part-time", sal.managerLevel3);
+        Employee emp4 = new Manager("Salma", "Full-time", sal.managerLevel2);
+        Employee emp5 = new Manager("Amitabh", "Full-time", sal.managerLevel2);
+        Employee emp6 = new Manager("Jitesh", "Part-time", sal.managerLevel3);
 
-        ArrayList<Object> employeeList= new ArrayList();
+        ArrayList<Employee> employeeList= new ArrayList();
         employeeList.add(emp1);
         employeeList.add(emp2);
         employeeList.add(emp3);
@@ -31,16 +31,10 @@ public class Main {
         employeeList.add(emp6);
 
        //CTC
-        ArrayList<Integer> salaryArray = new ArrayList();
-        salaryArray.add(emp1.salaryBand);
-        salaryArray.add(emp2.salaryBand);
-        salaryArray.add(emp3.salaryBand);
-        salaryArray.add(emp4.salaryBand);
-        salaryArray.add(emp5.salaryBand);
-        salaryArray.add(emp6.salaryBand);
+
         int sum = 0;
-        for(int i=0;i<salaryArray.size();i++){
-          sum=sum+salaryArray.get(i);
+        for(int i=0;i<employeeList.size();i++){
+          sum=sum+employeeList.get(i).salaryBand;
 
 
         }
